7a058ec16364eed6aa88cc12a45f2cf7acbe8a58
Osvaldo <osvaldoleivabarria1@gmail.com>
2020-10-02 07:32:53 -0300

    login!

--- src/main/java/com/example/todolist/controllers/UsuarioController.java
+++ src/main/java/com/example/todolist/controllers/UsuarioController.java
@@ -1,24 +1,29 @@
 package com.example.todolist.controllers;
 
+import com.example.todolist.dto.Token;
 import com.example.todolist.entitys.Usuario;
 import com.example.todolist.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository repository;
 
     @PostMapping("/login")
-    public String login(@RequestBody Usuario usuario) {
+    public Token login(@RequestBody Usuario usuario) {
         String user = usuario.getNombre();
         String password =  usuario.getPassword();
+        Token token = new Token();
         if(repository.findByNombreAndPassword(user,password) != null){
-            return "token";
+            token.setToken("token");
+            return token;
         }
         return null;
     }
---
+++ src/main/java/com/example/todolist/dto/Token.java
@@ -0,0 +1,10 @@
+package com.example.todolist.dto;
+
+import lombok.Data;
+
+@Data
+public class Token {
+
+    private String token;
+
+}
